c073400232f5cb20a71f37e14ae17def74a4457f
sahwa <ucbtsm8@ucl.ac.uk>
2022-04-27 16:39:54 +0100

    27 april

--- scripts/table.R
+++ scripts/table.R
@@ -1,9 +1,12 @@
 library(kableExtra)
 
 dat %>%
-	mutate(across(-target, function(x) as.numeric(round(x, digits=2)))) %>%
-	kbl(booktabs = T, format='latex', linesep = "", longtable=T) %>% 
-	kable_styling(position = "center", latex_options = c("repeat_header"), font_size = 6) %>%
-	kableExtra::landscape()
-
+	# # select(-Threshold, -`F-measure`) %>%
+	# mutate(across(where(is.numeric), function(x) as.numeric(round(x, digits=4)))) %>%
+	# select(ind, coverage, type, Precision, Sensitivity) %>%
+	# rename(Ind = ind, Coverage = coverage, Type = type) %>%
+	# arrange(Ind, Coverage, Type) %>%
+	# # mutate(across(1:4, function(x) formattable::comma(x, digits=0)))
+	kbl(booktabs = T, format='latex', linesep = "") %>% 
+	kable_styling(position = "center",  font_size = 8, full_width = F) 
 
